036a55b32f6ee0fb2c3d32efeae0516172ae7098
josephbrehm <64801701+josephbrehm@users.noreply.github.com>
2021-10-27 14:58:26 -0600

    Initial functions (fetch_dima and forget_who)

---
+++ forget_who.R
@@ -0,0 +1,11 @@
+#' Remove observer and recorder from a table
+#'
+#' @description Given a table, return it without observer, recorder, data enterer, or data checker
+#' @param data The table to edit
+#' @return A table without observer or recorder
+
+forget_who <- function(data){
+  out <- data %>% 
+    dplyr::select_if(!(names(.) %in% c("Observer", "Recorder", "DataEntry", "DataErrorChecking")))
+  return(out)
+}
