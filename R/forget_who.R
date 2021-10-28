bd1e5274edbeccac3429d6887df089b445d4c919
josephbrehm <64801701+josephbrehm@users.noreply.github.com>
2021-10-28 08:39:45 -0600

    update to remove rproj, change name of pipe, fetch, add name fields to forget

--- R/forget_who.R
+++ R/forget_who.R
@@ -1,13 +1,14 @@
-#' Remove observer and recorder from a table
+#' Remove observer, recorder, and similar from a table
 #'
-#' @description Given a table, return it without observer, recorder, data enterer, or data checker
+#' @description Given a table, return it without names of data collectors
 #' @param data The table to edit
-#' @return A table without observer or recorder
+#' @return A table without names
 
 #' @rdname forget_who
 #' @export forget_who
 forget_who <- function(data){
   out <- data %>% 
-    dplyr::select_if(!(names(.) %in% c("Observer", "Recorder", "DataEntry", "DataErrorChecking")))
+    dplyr::select_if(!(names(.) %in% c("Observer", "Recorder", "DataEntry", "DataErrorChecking",
+                                       "ESD_Investigators")))
   return(out)
 }
