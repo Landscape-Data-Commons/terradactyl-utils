859cfbc950094e8d355cba7dbd20cbd97f3ace27
josephbrehm <64801701+josephbrehm@users.noreply.github.com>
2021-12-14 10:39:09 -0700

    Create merge_indicators.R

---
+++ R/merge_indicators.R
@@ -0,0 +1,33 @@
+## this taken from the back half of build_indicators. 
+## add in all indicators columns from the "TerrADat" layer in a template GDB
+
+
+add_indicator_columns <- function(dsn_template,
+                             source,
+                             all_indicators){
+  feature_class_field_names <- 
+    sf::st_read(dsn_template, 
+                layer = dplyr::if_else(source %in% c("AIM", "TerrADat"), "TerrADat", source))
+  feature_class_field_names <- 
+    feature_class_field_names[,!colnames(feature_class_field_names) %in% 
+                                c("created_user", "created_date", 
+                                  "last_edited_user", "last_edited_date")]
+  indicator_field_names <- 
+    data.frame(name = names(all_indicators), 
+               calculated = "yes")
+  missing_names <- 
+    data.frame(name = names(feature_class_field_names), 
+               feature.class = "yes") %>% 
+    dplyr::full_join(indicator_field_names) %>% 
+    subset(is.na(calculated), select = "name") %>% 
+    dplyr::mutate(value = NA) %>% 
+    tidyr::spread(key = name, value = value) %>% 
+    dplyr::select(-Shape, 
+                  -GlobalID)
+  missing_names[nrow(all_indicators), ] <- NA
+  missing_names[, grepl(names(missing_names), pattern = "^FH|^AH|^Num")] <- 0
+  final_feature_class <- dplyr::bind_cols(all_indicators, 
+                                          missing_names)
+  
+  return(final_feature_class)
+}
